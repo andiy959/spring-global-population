9444e6ca9fb153625fd3038751801d6150b7b089
andiy959 <andell868@gmail.com>
2021-02-18 15:58:12 +0300

    initial commit

---
+++ src/main/java/com/demo/springglobalpopulation/SpringGlobalPopulationApplication.java
@@ -0,0 +1,13 @@
+package com.demo.springglobalpopulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGlobalPopulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGlobalPopulationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/springglobalpopulation/SpringGlobalPopulationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springglobalpopulation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGlobalPopulationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
